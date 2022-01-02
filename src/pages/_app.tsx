5819a7dbd060d280b9ad5ddddbe02226d5bfaef8
Tom <tomohiro3jp@gmail.com>
2022-01-02 21:15:14 +0900

    WIP: Making state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,32 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useReducer } from 'react';
 import Layout from '../components/templates/Layout';
 
 type CustomAppProps = AppProps & {
   data: Array<{ [key: string]: any }>;
 };
 
+const initialState = {
+  code: '',
+  name: '',
+  market: '',
+};
+
+function reducer(state: any, action: any) {
+  switch (action.type) {
+    case 'SET_FILTER':
+      return { ...state, [action.payload.key]: action.payload.value };
+    default:
+      return state;
+  }
+}
+
 function CustomApp({ Component, pageProps, data }: CustomAppProps) {
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <Layout initialData={data}>
-      <Component initialData={data} {...pageProps} />
+    <Layout initialData={data} dispatch={dispatch}>
+      <Component initialData={data} initialState={state} {...pageProps} />
     </Layout>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,17 @@ import type { NextPage } from 'next';
 import StockCard from '../components/organisms/StockCard';
 
 const Home: NextPage = (props: any) => {
+  const code = props.initialState.code;
+  const name = props.initialState.name;
+  const market = props.initialState.market;
+
+  const matchedStocks = props.initialData.filter(
+    (stock: any) => stock.code == code || stock.name === name || stock.market === market
+  );
   return (
     <>
       <Grid container spacing={2}>
-        {props.initialData.map((data: any) => (
+        {matchedStocks.map((data: any) => (
           <Grid item key={data.name} xs={12} md={6} lg={4}>
             <StockCard {...data} />
           </Grid>
