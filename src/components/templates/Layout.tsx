0f1ff9e67369091c8678108d73b6bf17b1515530
Tom <tomohiro3jp@gmail.com>
2022-02-05 01:29:03 +0900

    WIP: Making base layout component

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -1,210 +1,60 @@
-import Box from '@mui/material/Box';
-import Checkbox from '@mui/material/Checkbox';
-import Divider from '@mui/material/Divider';
-import Drawer from '@mui/material/Drawer';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import { useState } from 'react';
+import { css } from '@emotion/react';
+import ShowChartTwoToneIcon from '@mui/icons-material/ShowChartTwoTone';
+import SummarizeTwoToneIcon from '@mui/icons-material/SummarizeTwoTone';
+import IconButton from '@mui/material/IconButton';
+import Link from 'next/link';
 
-import { MARKETS, INDUSTRY_33, VALUE_OR_GROWTH, IS_PRICE_SHIFTABLE, GROUPS } from '../../constants/seacrh-filters';
-import SearchAutocomplete from '../organisms/SearchAutocomplete';
-import PlainAccordion from '../primitives/Accordion';
-
-const drawerWidth = 240;
-
-function ResponsiveDrawer(props: any) {
-  const { window } = props;
-  const [mobileOpen, setMobileOpen] = useState(false);
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  const drawer = (
-    <div>
-      <List>
-        <SearchAutocomplete
-          initiallyFetchedData={props.initiallyFetchedData}
-          setStockCodeName={(name: string, code: string) =>
-            props.dispatch({ type: 'SET_STOCK_CODE_NAME', payload: { name, code } })
-          }
-        />
-      </List>
-      <Divider />
-      <List>
-        <ListItem>
-          <PlainAccordion name={MARKETS.name}>
-            <FormGroup>
-              {MARKETS.options.map((market) => (
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      onChange={(event) =>
-                        event.target.checked
-                          ? props.dispatch({
-                              type: 'SET_MARKET',
-                              payload: market,
-                            })
-                          : props.dispatch({
-                              type: 'DELETE_MARKET',
-                              payload: market,
-                            })
-                      }
-                    />
-                  }
-                  label={market}
-                  key={market}
-                />
-              ))}
-            </FormGroup>
-          </PlainAccordion>
-        </ListItem>
-        <ListItem>
-          <PlainAccordion name={GROUPS.name}>
-            <FormGroup>
-              {GROUPS.options.map((group) => (
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      onChange={(event) =>
-                        event.target.checked
-                          ? props.dispatch({
-                              type: 'SET_GROUP',
-                              payload: group,
-                            })
-                          : props.dispatch({
-                              type: 'DELETE_GROUP',
-                              payload: group,
-                            })
-                      }
-                    />
-                  }
-                  label={group}
-                  key={group}
-                />
-              ))}
-            </FormGroup>
-          </PlainAccordion>
-        </ListItem>
-        <ListItem>
-          <PlainAccordion name={INDUSTRY_33.name}>
-            <FormGroup>
-              {INDUSTRY_33.options.map((industry) => (
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      onChange={(event) =>
-                        event.target.checked
-                          ? props.dispatch({
-                              type: 'SET_INDUSTRY',
-                              payload: industry,
-                            })
-                          : props.dispatch({
-                              type: 'DELETE_INDUSTRY',
-                              payload: industry,
-                            })
-                      }
-                    />
-                  }
-                  label={industry}
-                  key={industry}
-                />
-              ))}
-            </FormGroup>
-          </PlainAccordion>
-        </ListItem>
-        <ListItem>
-          <PlainAccordion name={VALUE_OR_GROWTH.name}>
-            <RadioGroup defaultValue="None">
-              {VALUE_OR_GROWTH.options.map((option) => (
-                <FormControlLabel
-                  control={
-                    <Radio
-                      onChange={() => {
-                        props.dispatch({
-                          type: 'SET_VALUEORGROWTH',
-                          payload: option.value,
-                        });
-                      }}
-                    />
-                  }
-                  label={option.label}
-                  value={option.label}
-                  key={option.label}
-                />
-              ))}
-            </RadioGroup>
-          </PlainAccordion>
-        </ListItem>
-        <ListItem>
-          <PlainAccordion name={IS_PRICE_SHIFTABLE.name}>
-            <RadioGroup defaultValue="None">
-              {IS_PRICE_SHIFTABLE.options.map((option) => (
-                <FormControlLabel
-                  control={
-                    <Radio
-                      onChange={() => {
-                        props.dispatch({
-                          type: 'SET_IS_PRICESHIFTABLE',
-                          payload: option.value,
-                        });
-                      }}
-                    />
-                  }
-                  label={option.label}
-                  value={option.label}
-                  key={option.label}
-                />
-              ))}
-            </RadioGroup>
-          </PlainAccordion>
-        </ListItem>
-      </List>
-      <Divider />
-    </div>
-  );
-
-  const container = window !== undefined ? () => window().document.body : undefined;
-
+function Layout(props: any) {
   return (
-    <Box sx={{ display: 'flex' }}>
-      <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }} aria-label="mailbox folders">
-        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-        <Drawer
-          container={container}
-          variant="temporary"
-          open={mobileOpen}
-          onClose={handleDrawerToggle}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
-      <Box mt={4} component="main" sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}>
-        {props.children}
-      </Box>
-    </Box>
+    <div css={container}>
+      <div css={activityBar}>
+        <div css={buttons}>
+          <IconButton
+            sx={{ p: 0 }}
+            aria-label="delete"
+            size="large"
+            disableRipple
+            // LinkComponent={<Link href="/stock" />}
+          >
+            <SummarizeTwoToneIcon fontSize="inherit" />
+          </IconButton>
+          <IconButton sx={{ p: 0 }} aria-label="delete" size="large" disableRipple>
+            <ShowChartTwoToneIcon fontSize="inherit" />
+          </IconButton>
+        </div>
+      </div>
+      <main css={main}>{props.children}</main>
+    </div>
   );
 }
 
-export default ResponsiveDrawer;
+export default Layout;
+
+const activityBarWidth = '80px';
+
+const container = css({
+  display: 'flex',
+});
+
+const activityBar = css({
+  width: activityBarWidth,
+  height: '100vh',
+  padding: 8,
+  overflowY: 'auto',
+});
+
+const buttons = css({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  '& :not(:last-child)': {
+    marginBottom: '10px',
+  },
+});
+
+const main = css({
+  display: 'flex',
+  flexGrow: 1,
+  padding: 8,
+  width: `calc(100% - ${activityBarWidth}px)`,
+});
