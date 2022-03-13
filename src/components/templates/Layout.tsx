67d62ed45d5610de9c410cdd53a3423d31e44ebd
Tom <tomohiro3jp@gmail.com>
2022-03-13 16:02:16 +0900

    WIP:親レイアウトコンポネントレスポンシブ化

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -2,27 +2,30 @@ import { css } from '@emotion/react';
 import ShowChartTwoToneIcon from '@mui/icons-material/ShowChartTwoTone';
 import SummarizeTwoToneIcon from '@mui/icons-material/SummarizeTwoTone';
 import IconButton from '@mui/material/IconButton';
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import Link from 'next/link';
 
 function Layout(props: any) {
+  const theme = useTheme();
+  const isBigSize = useMediaQuery(theme.breakpoints.up('sm'));
   return (
     <div css={container}>
-      <div css={activityBar}>
-        <div css={buttons}>
+      <div css={activityBar(isBigSize)}>
+        <div css={links(isBigSize)}>
           <IconButton
-            sx={{ p: 0 }}
-            aria-label="delete"
-            size="large"
+            aria-label="to-stock"
             disableRipple
             // LinkComponent={<Link href="/stock" />}
           >
-            <SummarizeTwoToneIcon fontSize="inherit" />
+            <SummarizeTwoToneIcon fontSize="large" sx={{ p: 0 }} />
           </IconButton>
-          <IconButton sx={{ p: 0 }} aria-label="delete" size="large" disableRipple>
-            <ShowChartTwoToneIcon fontSize="inherit" />
+          <IconButton aria-label="to-chart" size="large" disableRipple>
+            <ShowChartTwoToneIcon fontSize="large" sx={{ p: 0 }} />
           </IconButton>
         </div>
       </div>
+      {/* {isBigSize ? <Divider orientation="vertical" /> : null} */}
       <main css={main}>{props.children}</main>
     </div>
   );
@@ -30,31 +33,52 @@ function Layout(props: any) {
 
 export default Layout;
 
-const activityBarWidth = '80px';
+const activityBarWidthHeight = '60px';
 
 const container = css({
   display: 'flex',
-});
-
-const activityBar = css({
-  width: activityBarWidth,
   height: '100vh',
-  padding: 8,
-  overflowY: 'auto',
 });
 
-const buttons = css({
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  '& :not(:last-child)': {
-    marginBottom: '10px',
-  },
-});
+const activityBar = (isBig: boolean) =>
+  css`
+    background-color: #eee;
+    ${isBig
+      ? `
+      min-width: ${activityBarWidthHeight};
+      padding: 8px 10px;
+      overflow-y: auto;
+      `
+      : `
+      position: fixed;
+      bottom: 0;
+      width: 100%;
+      height: ${activityBarWidthHeight};
+      padding: 10px 8px;
+      overflow-x: auto;
+      z-index: 2;
+    `}
+  `;
+
+const links = (isBig: boolean) => css`
+  display: flex;
+  justify-content: center;
+  ${isBig
+    ? `
+    flex-direction: column;
+    & :not(:last-child): {
+      margin-bottom: 10px,
+      `
+    : `
+    height: 100%;
+    align-items: base;
+     & :not(:last-child): {
+        margin-right: 8px,
+   `}
+`;
 
 const main = css({
   display: 'flex',
-  flexGrow: 1,
-  padding: 8,
-  width: `calc(100% - ${activityBarWidth}px)`,
+  padding: '8px 10px',
+  width: `calc(100% - ${activityBarWidthHeight}px)`,
 });
